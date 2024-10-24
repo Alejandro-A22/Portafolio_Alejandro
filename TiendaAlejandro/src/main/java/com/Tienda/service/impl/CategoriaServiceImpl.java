4e5cdf31fd5fc3db3e6125a4aba6a9b2773810b1
Alejandro1103 <alejaf2211@gmail.com>
2024-10-23 20:36:42 -0600

    Commit 5
    Commit semana 6
    CRUD // Firebase

--- TiendaAlejandro/src/main/java/com/Tienda/service/impl/CategoriaServiceImpl.java
+++ TiendaAlejandro/src/main/java/com/Tienda/service/impl/CategoriaServiceImpl.java
@@ -1,8 +1,8 @@
-package com.tienda.service.impl;
+package com.Tienda.service.Impl;
 
 import com.Tienda.dao.CategoriaDao;
 import com.Tienda.domain.Categoria;
-import com.tienda.service.CategoriaService;
+import com.Tienda.service.CategoriaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,17 +10,35 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class CategoriaServiceImpl implements CategoriaService {
-    
+
     @Autowired
     private CategoriaDao categoriaDao;
 
     @Override
-    @Transactional(readOnly=true)
+    @Transactional(readOnly = true)
     public List<Categoria> getCategorias(boolean activos) {
-        var lista=categoriaDao.findAll();
+        var lista = categoriaDao.findAll();
         if (activos) {
-           lista.removeIf(e -> !e.isActivo());
+            lista.removeIf(e -> !e.isActivo());
         }
         return lista;
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Categoria getCategoria(Categoria categoria) {
+        return categoriaDao.findById(categoria.getIdCategoria()).orElse(null);
+    }
+
+    @Override
+    @Transactional
+    public void save(Categoria categoria) {
+        categoriaDao.save(categoria);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Categoria categoria) {
+        categoriaDao.delete(categoria);
     }
 }
