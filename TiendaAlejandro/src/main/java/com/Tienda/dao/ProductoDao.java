ed2f88d8cfcea5dc578cd03eae9285ee1f1d6d13
Alejandro1103 <alejaf2211@gmail.com>
2024-11-06 20:41:49 -0600

    Commit 5
    Creacion Productos y Pruebas

---
+++ TiendaAlejandro/src/main/java/com/Tienda/dao/ProductoDao.java
@@ -0,0 +1,11 @@
+
+package com.Tienda.dao;
+import com.Tienda.domain.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+/**
+ *
+ * @author Alejandro
+ */
+public interface ProductoDao {
+    
+}
